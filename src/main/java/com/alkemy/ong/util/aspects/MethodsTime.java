bd0d040fb3b160894a580bf55bdd1ab3d5a554d4
Maxi <maxifop96@gmail.com>
2022-09-12 13:17:41 -0300

    Aspect method was added.

--- src/main/java/com/alkemy/ong/util/aspects/MethodsTime.java
+++ src/main/java/com/alkemy/ong/util/aspects/MethodsTime.java
@@ -1,32 +1,24 @@
 package com.alkemy.ong.util.aspects;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Aspect
 @Component
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class MethodsTime {
 
-    @Around("execution(* com.alkemy.ong.*.*(..))")
-    public Object calculateTime(ProceedingJoinPoint joinPoint) throws Throwable {
-
-        long t1 = System.currentTimeMillis();
-
-        Object result = joinPoint.proceed();
-
-        long t2 = System.currentTimeMillis();
-
-        if( t2-t1>100) {
-
-            System.out.println("Metodo lento:"+ joinPoint.getTarget().getClass()+"."+joinPoint.getSignature().getName() +":"+ (t2-t1));
-        }
-
-        return result;
-
+    @AfterReturning("execution(* com.alkemy.ong.service.impl.*.*(..))")
+    public void afterSuccessfullyReturning (JoinPoint joinPoint){
+        System.out.println("Method: " +
+                joinPoint.getTarget().getClass().getSimpleName() + "." +
+                joinPoint.getSignature().getName());
+        System.out.println("Response Time: " + LocalDateTime.now());
     }
+
+
 
 }
