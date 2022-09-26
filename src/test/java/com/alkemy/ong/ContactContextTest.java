05fda4a5d307d67eac6ef3d6b77a33a4d5b40c50
Maxi <maxifop96@gmail.com>
2022-09-26 12:20:48 -0300

    Cleaning code

--- src/test/java/com/alkemy/ong/ContactContextTest.java
+++ src/test/java/com/alkemy/ong/ContactContextTest.java
@@ -5,14 +5,15 @@ import com.alkemy.ong.model.Contact;
 import com.alkemy.ong.repository.ContactRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = InMemoryUserDetails.class)
 @AutoConfigureMockMvc
 public class ContactContextTest {
