a9796f904ae7ffba5d462c0232202c9976be2a0b
Maxi Falcone <maxifop96@gmail.com>
2022-09-27 22:02:42 -0300

    Testing

--- src/test/java/com/alkemy/ong/OrganizationContextTest.java
+++ src/test/java/com/alkemy/ong/OrganizationContextTest.java
@@ -6,15 +6,11 @@ import com.alkemy.ong.model.Organization;
 import com.alkemy.ong.repository.OrganizationRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = InMemoryUserDetails.class)
 @AutoConfigureMockMvc
 public class OrganizationContextTest {
